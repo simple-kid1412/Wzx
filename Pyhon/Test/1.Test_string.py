7708d86de2d991e2ed14759af694852e5b3a6b51
wzx <suannaixigua@126.com>
2024-01-05 16:43:08 +0800

    20240105 补充经济,python学习的思维导图. 学习了新数据类型:字典和其使用方法

--- Pyhon/Test/1.Test_string.py
+++ Pyhon/Test/1.Test_string.py
@@ -1,6 +1,5 @@
-#使用if 输出想要的车型
-cars=["benchi","baoma","aodi"]
-if not "benchi" in cars:
-    print("is in ")
-print("这是一个pull 拉取的测试"   )
-print("
+satff={
+    'wzx':2,
+    'ckk':3,
+    'zjl':4,
+}
