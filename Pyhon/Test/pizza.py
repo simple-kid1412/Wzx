7398331e58367531cc96b4e10563bdfd0d599008
wzx <suannaixigua@126.com>
2024-01-09 20:43:56 +0800

    学习了python的类

--- Pyhon/Test/pizza.py
+++ Pyhon/Test/pizza.py
@@ -1,13 +1,23 @@
-class Restaurant:
-    def __init__(self,restaurant_name,cuisine_type):
-        self.restaurant_name=restaurant_name
-        self.cuisine_type=cuisine_type
-    def describe_resaurant(self):
-        print("our resautant is best")
-    def open_resautant(self):
-        print("can guan zhengzai yingye ")
-
-shuyuan=Restaurant('beijingshuyuan','chuancai')
-shuyuan.describe_resaurant()
-shuyuan.open_resautant()
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+    def get_descriptive_name(self):
+        #描述汽车基本属性
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+    def read_odometer(self):
+        #打印里程数
+        print(f"This car has {self.odometer_reading} miles on it.")
+    def update_odometer(self, mileage):
+        #设置里程数
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+    def increment_odometer(self, miles):
+        #让里程数增加指定的数值
+        self.odometer_reading += miles
 
