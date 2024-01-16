1c4a308a66fa3f6c99dd6250c53c8658ef4ca070
Simple <suannaixigua@126.com>
2024-01-16 20:23:24 +0800

    1

---
+++ Pyhon/Test/1_彩票中奖模拟.py
@@ -0,0 +1,24 @@
+from random import randint
+from random import choice
+list=list(range(0,10))
+a=choice(list)
+b=choice(list)
+c=choice(list)
+d=choice(list)
+num=0
+my_tickect=[2,3,0,8]
+while(1):
+    num+=1
+    a=choice(list)
+    b=choice(list)
+    c=choice(list)
+    d=choice(list)
+    print(f"本期的彩票中将号码是{a},{b},{c},{d}")
+    if a==my_tickect[0] and b==my_tickect[1] and c==my_tickect[2] and d==my_tickect[3]:
+        print(f"你中大奖了,在之前,已经开奖{num}次了")
+        break
+    else:
+        print("您未中奖,请再接再励")
+
+
+        
