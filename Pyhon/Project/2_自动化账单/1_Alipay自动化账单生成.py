a44380dd2a89529dfd2e7c130c5e67358a57d502
Simple <suannaixigua@126.com>
2024-02-22 22:51:30 +0800

    1

--- Pyhon/Project/2_自动化账单/1_Alipay自动化账单生成.py
+++ Pyhon/Project/2_自动化账单/1_Alipay自动化账单生成.py
@@ -1,5 +1,5 @@
 import csv
-
+import pymysql
 file='alipay_record_20240222_203230.csv'
 with open(file) as csvFile:          
     csv_reader = csv.reader(csvFile)    #读文件 生成一个 csv文件的READER对象
@@ -19,6 +19,30 @@ for i in range(0,len(detail_data)):
     print(detail_data[i])
 
 #将列表插入到本地数据库
+db = pymysql.connect(host='localhost', user='root',password='0322',database='alipay',charset='utf8mb4')
+cursor = db.cursor()
+#执行代码 插入所有数据
+sql = "INSERT INTO journal (交易时间, 交易分类,交易对方,商品说明,收/支,金额,商家订单号,备注) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+cursor.executemany(sql, detail_data)
+
+db.commit()
 
 
 
+#执行sql文件,对插入数据进行数据清洗
+conn = pymysql.connect(host='localhost',
+                       user='root', 
+                       password='password',
+                       database='dbname',
+                       charset='utf8mb4')
+
+cursor = conn.cursor()
+
+with open('script.sql') as f:
+  sql = f.read()
+  commands = sql.split(';')
+  for command in commands:
+    if command.strip():
+      cursor.execute(command)
+
+conn.commit()
